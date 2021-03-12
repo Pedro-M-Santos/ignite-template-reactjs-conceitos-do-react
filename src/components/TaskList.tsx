c58a28f93ade1a91ada1375e4283c0d0933b5896
MatiasPedro25 <unbmatiaspedro@gmail.com>
2021-03-12 10:52:05 -0300

    Desafio completo

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -16,14 +16,33 @@ export function TaskList() {
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if(newTaskTitle){
+      setTasks([...tasks,{
+        id: Math.round(Math.random()*100),
+        title:newTaskTitle,
+        isComplete:false
+      }])
+      setNewTaskTitle("")
+    }
+    else{
+      alert('You shall not add a task with an empty title!')
+    }
+
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    let indexOfEditedTask = tasks.map(task=>task.id).indexOf(id)
+    let tempTasksArray = [...tasks]
+    tempTasksArray[indexOfEditedTask].isComplete = !tempTasksArray[indexOfEditedTask].isComplete
+    setTasks(tempTasksArray)
+    // console.log(indexOfEditedTask)
+
   }
 
   function handleRemoveTask(id: number) {
-    // Remova uma task da listagem pelo ID
+    let filteredTasks = tasks.filter(task => task.id !== id)
+    setTasks(filteredTasks)
   }
 
   return (
